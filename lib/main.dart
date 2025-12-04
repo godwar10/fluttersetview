ecfeb689b88136e6b4fd27e2f5c90c7962b66b52
高高叔叔 <352829617@qq.com>
2025-12-04 15:26:34 +0800

    提交更新

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '设置页面',
+      title: '设置页面1',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
